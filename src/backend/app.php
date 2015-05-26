1ae7e59b1a2cab88b0a4fa76fb01b737c95914d9
hitsugaya95 <jimmy.phimmasone@gmail.com>
2015-05-27 01:00:20 +0200

    Add custom error page

--- src/backend/app.php
+++ src/backend/app.php
@@ -88,10 +88,10 @@ $app->error(function (\Exception $e, $code) use ($app) {
 
     switch ($code) {
         case 404:
-            $message = 'The requested page could not be found.';
+            $message = $app['twig']->render('errors/404.html', array());
             break;
         default:
-            $message = 'We are sorry, but something went terribly wrong.';
+            $message = $app['twig']->render('errors/500.html', array());
     }
 
     return new Response($message, $code);
